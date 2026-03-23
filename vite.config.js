674530b3365fcdc81775877dfc7b3817cf758114
Anom-a <anomabebe639@gmail.com>
2026-03-24 01:30:33 +0300

    added optimizer dependencry for fast loading

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePrerender } from 'vite-plugin-prerender'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
@@ -13,12 +12,6 @@ export default defineConfig({
       jpeg: { quality: 80 },
       jpg: { quality: 80 },
       webp: { quality: 82 },
-    }),
-    VitePrerender({
-      routes: ['/'],
-      rendererOptions: {
-        renderAfterDocumentEvent: 'render-event',
-      },
     }),
   ],
 })
